3a83b2102ed605d4739731d7e1bad5c5d6ca94b6
Andrew Mitchell <a.j.mitchell@ucl.ac.uk>
2026-02-25 00:59:54 +0000

    Fully working devcontainer with renv packages!

---
+++ .devcontainer/init-env.sh
@@ -0,0 +1,6 @@
+uv sync
+Rscript -e 'install.packages("renv")'
+Rscript -e 'renv::restore(prompt=FALSE)'
+Rscript -e 'tinytex::install_tinytex()'
+source .venv/bin/activate
+quarto check
--- setup-r-lib.sh
+++
@@ -1,2 +0,0 @@
-source .venv/bin/activate
-export LD_LIBRARY_PATH="$(python -m rpy2.situation LD_LIBRARY_PATH)":${LD_LIBRARY_PATH}
